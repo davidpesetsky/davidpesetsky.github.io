3650d41d0d8deba8eee2aa8446e4eaf28430170b
David Pesetsky <pesetsk@mit.edu>
2026-05-20 08:54:31 -0400

    Fix purgecss config syntax error

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,8 +3,12 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
-  safelist: {
-    standard: ['bibliography', 'star-button', 'abbr', 'links', 'btn', 'col-sm-2'],
-    greedy: [/^bibliography/, /^star-button/]
-  },
+  safelist: [
+    /bibliography/,
+    /star-button/,
+    /abbr/,
+    /links/,
+    /btn/,
+    /col-sm-2/,
+  ],
 };
